f672c2823e26b1f5c5b81a690036becaf434bd62
14-Kanokkorn-Kerdsomboon <nanofeerfier@gmail.com>
2024-08-30 14:30:48 +0700

    my-auglar-project-1HttpClients commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { provideHttpClient } from '@angular/common/http';
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -11,3 +9,4 @@ export const appConfig: ApplicationConfig = {
      provideHttpClient()
     ]
 };
+// my-auglar-project-1HttpClients
